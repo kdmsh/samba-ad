125783f39c8362006b9b278d40ad0d1e825e59a3
kdmsh <kaneshige@kodama-system.com>
2024-02-17 15:38:36 +0900

    Optimize dash

--- install.sh
+++ install.sh
@@ -1,21 +1,8 @@
-#!/bin/bash
-# シェル名が"bash"かどうかをチェック
-# プロセスの情報を表示
-CMD=$(ps -o command= -p $$)
-echo "自プロセス：$CMD"
-#if [ "$CMD" =~ "bash" ]; then
-if echo "$CMD" | grep "bash"  ; then
-  echo "bashです"
-else
-  echo "このスクリプトはbash以外で実行されています"
-  /bin/bash "$0" "$@" 
-  exit $?
-fi
-
+#!/bin/sh
 # スクリプトを実行するユーザーがrootであることを確認
-WHO=$(whoami)
-echo $WHO
-if [[ "$WHO" != "root" ]]; then
+ID=$(id -u)
+if [ "$ID" != 0 ]; then
+  echo $ID
   echo "このスクリプトはrootユーザーで実行する必要があります。"
   sudo /bin/bash "$0" "$@"
   exit $?
@@ -24,12 +11,12 @@ fi
 # Sambaをインストール
 apt update
 PGK=""
-apt list --installed | grep samba      || PKG="$PKG samba"
-apt list --installed | grep  krb5-user || PKG="$PKG krb5-user"
-apt list --installed | grep realmd     || PKG="$PKG realmd"
+apt list --installed | grep samba     || PKG="$PKG samba"
+apt list --installed | grep krb5-user || PKG="$PKG krb5-user"
+apt list --installed | grep realmd    || PKG="$PKG realmd"
 
-num_words=$(wc -w <<< "$PKG")
-if [[ $num_words -gt 1 ]]; then
+num_words=$(echo $PKG | wc -w )
+if [ $num_words -gt 1 ]; then
   echo apt install -y $PKG
   apt install -y $PKG
 fi
