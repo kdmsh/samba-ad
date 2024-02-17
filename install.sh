7387b9ebf72d3e23a0ff36fa9f731bdd89999750
kdmsh <kaneshige@kodama-system.com>
2024-02-17 16:39:18 +0900

    パッケージのチェック方法を変更

--- install.sh
+++ install.sh
@@ -8,13 +8,43 @@ if [ "$ID" != 0 ]; then
   exit $?
 fi
 
-# Sambaをインストール
-apt update
-PGK=""
-apt list --installed | grep samba     || PKG="$PKG samba"
-apt list --installed | grep krb5-user || PKG="$PKG krb5-user"
-apt list --installed | grep realmd    || PKG="$PKG realmd"
+echo -n "ホスト名[dc01]: "
+read HOST
+if [ "$HOST" = "" ]; then
+  HOST="dc01"
+fi
+hostnamectl set-hostname $HOST
 
+echo -n "ドメイン[ad.kodama-system.com]: "
+read DOMAIN
+if [ "$DOMAIN" = "" ]; then
+  DOMAIN="ad.kodama-system.com"
+fi
+
+# tailscaleのインストール
+if [ ! -x /usr/bin/tailscale ];then
+	curl -fsSL https://tailscale.com/install.sh | sh
+	tailscale up
+	tailscale up --ssh
+fi
+
+IP=$(/usr/bin/tailscale status | grep `hostname` | cut -d" " -f1)
+if ! grep $HOST.$DOMAIN /etc/hosts >/dev/null 2>&1 ;then
+	echo "$IP	$HOST.$DOMAIN	$HOST" > /etc/hosts
+fi
+
+# DNSを停止
+systemctl disable systemd-resolved.service
+
+# Sambaをインストール
+apt -q update
+PGK=""
+for P in acl attr dnsutils krb5-config krb5-user samba samba-dsdb-modules samba-vfs-modules smbclient winbind;do
+	apt -q list --installed | grep $P >/dev/null 2>&1 || PKG="$PKG $P"
+done
+echo "インストール対象のパッケージ"
+echo $PKG
+exit 0
 num_words=$(echo $PKG | wc -w )
 if [ $num_words -gt 1 ]; then
   echo apt install -y $PKG
