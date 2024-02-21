96b503dac771788b4b9584f17f45efe919876db3
kdmsh <160145510+kdmsh@users.noreply.github.com>
2024-02-21 22:55:04 +0900

    完成したかな？ベータ版ってことで。

--- config.sh
+++ config.sh
@@ -5,11 +5,11 @@ lang="ja_JP.UTF-8"
 ipv6=true
 fqdn=dc01.ad.kodama-system.com
 
+# Common Settings
+tailscale=false
+
 # Samba Setting
 REALM="AD.KODAMA-SYSTEM.COM"
 domain="AD"
-server_role="dc"
-dns_backend="SAMBA_INTERNAL"
-dns_forwarder="8.8.8.8"
 admin_password="55Kodam@"
 
--- install.sh
+++ install.sh
@@ -7,6 +7,103 @@ set -x
 . ./config.sh
 shopt -s expand_aliases
 
+function enter_to_next(){
+  echo "続けるにはEnterキーを押してください..."
+  read
+}
+
+function service_start(){
+  service_name=$@
+  # サービスのマスク状態を確認
+  systemctl is-masked $service_name > /dev/null 2>&1
+
+  # サービスがマスクされている場合
+  if [ $? -eq 0 ]; then
+    # サービスのマスクを解除
+    echo "サービス[$service_name]のマスクを解除しています..."
+    systemctl unmask $service_name
+
+    # サービスのマスク解除を確認
+    systemctl is-masked $service_name > /dev/null 2>&1
+
+    # サービスがマスク解除されている場合
+    if [ $? -ne 0 ]; then
+      echo "サービス[$service_name]のマスク解除に成功しました。"
+    else
+      echo "サービス[$service_name]のマスク解除に失敗しました。"
+    fi
+  else
+    echo "サービス[$service_name]はマスクされていないため、マスク解除処理はスキップされます。"
+  fi
+
+  # サービスの有効状態を確認
+  systemctl is-enabled $service_name > /dev/null 2>&1
+
+  # サービスが無効化されている場合
+  if [ $? -ne 0 ]; then
+    # サービスを有効化
+    echo "サービス[$service_name]を有効化しています..."
+    systemctl enable $service_name
+
+    # サービスの有効化を確認
+    systemctl is-enabled $service_name > /dev/null 2>&1
+
+    # サービスが有効化されている場合
+    if [ $? -eq 0 ]; then
+      echo "サービス[$service_name]の有効化に成功しました。"
+    else
+      echo "サービス[$service_name]の有効化に失敗しました。"
+    fi
+  else
+    echo "サービス[$service_name]は既に有効化されているため、有効化処理はスキップされます。"
+  fi
+  # サービスの起動状態を確認
+  systemctl is-active $service_name > /dev/null 2>&1
+
+  # サービスが停止している場合
+  if [ $? -ne 0 ]; then
+    # サービスを起動
+    echo "サービス[$service_name]を起動しています..."
+    systemctl start $service_name
+
+    # サービスの起動を確認
+    systemctl is-active $service_name > /dev/null 2>&1
+
+    # サービスが起動している場合
+    if [ $? -eq 0 ]; then
+      echo "サービス[$service_name]の起動に成功しました。"
+    else
+      echo "サービス[$service_name]の起動に失敗しました。"
+    fi
+  else
+    echo "サービス[$service_name]は既に起動しているため、起動処理はスキップされます。"
+  fi
+}
+
+function service_stop(){
+  for service_name in $@;do
+    systemctl status $service_name > /dev/null 2>&1
+    # サービスが起動している場合
+    if [ $? -eq 0 ]; then
+      # サービスを停止
+      echo "サービス[$service_name]を停止しています..."
+      systemctl stop $service_name
+      systemctl disable $service_name
+    
+      # サービスの停止を確認
+      systemctl status $service_name > /dev/null 2>&1
+    
+      # サービスが停止した場合
+      if [ $? -ne 0 ]; then
+        echo "サービス[$service_name]の停止に成功しました。"
+      else
+        echo "サービス[$service_name]の停止に失敗しました。"
+      fi
+    else
+      echo "サービス[$service_name]は起動していないため、停止処理はスキップされます。"
+    fi
+  done
+}
 # スクリプトを実行するユーザーがrootであることを確認
 ID=$(id -u)
 if [ "$ID" != 0 ]; then
@@ -41,6 +138,7 @@ if [ -n "$fqdn" ]; then
 fi
 hostnamectl set-hostname ${fqdn%%.*}
 
+
 # timezone
 ln -fs "/usr/share/zoneinfo/$timezone" /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
@@ -51,6 +149,7 @@ if [ $lang != "none" ] && [ $lang != $LANG ]; then
   pkg_mgr_install language-pack-ja language-pack-ja-base && update-locale LANG=$lang
 fi
 
+
 # IPv6
 if "${ipv6}"; then
   if [ -f /etc/netplan/01-netcfg.yaml ]; then
@@ -60,14 +159,17 @@ if "${ipv6}"; then
   fi
 fi
 
+
 # tailscaleのインストール
-if [ ! -x /usr/bin/tailscale ];then
-	curl -fsSL https://tailscale.com/install.sh | sh
-	tailscale up
-	tailscale up --ssh
+if "${tailscale}"; then
+  if [ ! -x /usr/bin/tailscale ];then
+    curl -fsSL https://tailscale.com/install.sh | sh
+    tailscale up
+    tailscale up --ssh
+  fi
 fi
 
-exit
+enter_to_next
 
 #IP=$(/usr/bin/tailscale status | grep `hostname` | cut -d" " -f1)
 #IP=$(/usr/bin/tailscale ip |head -1)
@@ -78,6 +180,7 @@ exit
 # DNSの設定変更
 systemctl disable systemd-resolved.service
 
+enter_to_next
 
 # Sambaをインストール
 PGK="acl attr dnsutils krb5-config krb5-user samba samba-dsdb-modules samba-vfs-modules smbclient winbind"
@@ -94,6 +197,10 @@ echo "インストール対象のパッケージ"
 #  apt install -y $PKG
 #fi
 apt-get -y install $PKG
+
+echo "パッケージのインストールが完了しました"
+enter_to_next
+
 # 設定ファイルの退避
 if [ ! -f /etc/resolv.conf.org ];then
 	mv /etc/resolv.conf /etc/resolv.conf.org
@@ -106,12 +213,15 @@ if [ ! -f /etc/krb5.conf.org ];then
 	mv /etc/krb5.conf /etc/krb5.conf.org
 fi
 
+echo "次はsamba-toolの設定です"
+enter_to_next
+
 samba-tool domain provision \
   --use-rfc2307 \
   --realm="$REALM" \
   --domain="$domain" \
-  --server-role="$server_role" \
-  --dns-backend="$dns_backend" \
+  --server-role="dc" \
+  --dns-backend="SAMBA_INTERNAL" \
   --option="dns forwarder=8.8.8.8" \
   --option="dns forwarder=8.8.4.4" \
   --adminpass="$admin_password"
@@ -120,111 +230,19 @@ cp /var/lib/samba/private/krb5.conf /etc/
 
 # 不要なサービスの無効化
 #systemctl stop smbd.service nmbd.service winbind.service
-for service_name in smbd.service nmbd.service winbind.service;do
-	systemctl status $service_name > /dev/null 2>&1
-	# サービスが起動している場合
-  if [ $? -eq 0 ]; then
-    # サービスを停止
-	  echo "サービス[$service_name]を停止しています..."
-	  systemctl stop $service_name
-		systemctl disable $service_name
-	
-	  # サービスの停止を確認
-	  systemctl status $service_name > /dev/null 2>&1
-	
-	  # サービスが停止した場合
-	  if [ $? -ne 0 ]; then
-	    echo "サービス[$service_name]の停止に成功しました。"
-	  else
-	    echo "サービス[$service_name]の停止に失敗しました。"
-	  fi
-	else
-	  echo "サービス[$service_name]は起動していないため、停止処理はスキップされます。"
-	fi
-done
+service_stop smbd.service nmbd.service winbind.service
 
 # ADサービスの有効化
-#systemctl unmask samba-ad-dc.service
-# サービス名
-service_name="samba-ad-dc.service"
-
-# サービスのマスク状態を確認
-systemctl is-masked $service_name > /dev/null 2>&1
-
-# サービスがマスクされている場合
-if [ $? -eq 0 ]; then
-  # サービスのマスクを解除
-  echo "サービス[$service_name]のマスクを解除しています..."
-  systemctl unmask $service_name
-
-  # サービスのマスク解除を確認
-  systemctl is-masked $service_name > /dev/null 2>&1
-
-  # サービスがマスク解除されている場合
-  if [ $? -ne 0 ]; then
-    echo "サービス[$service_name]のマスク解除に成功しました。"
-  else
-    echo "サービス[$service_name]のマスク解除に失敗しました。"
-  fi
-else
-  echo "サービス[$service_name]はマスクされていないため、マスク解除処理はスキップされます。"
-fi
-
-# サービスの有効状態を確認
-systemctl is-enabled $service_name > /dev/null 2>&1
-
-# サービスが無効化されている場合
-if [ $? -ne 0 ]; then
-  # サービスを有効化
-  echo "サービス[$service_name]を有効化しています..."
-  systemctl enable $service_name
-
-  # サービスの有効化を確認
-  systemctl is-enabled $service_name > /dev/null 2>&1
-
-  # サービスが有効化されている場合
-  if [ $? -eq 0 ]; then
-    echo "サービス[$service_name]の有効化に成功しました。"
-  else
-    echo "サービス[$service_name]の有効化に失敗しました。"
-  fi
-else
-  echo "サービス[$service_name]は既に有効化されているため、有効化処理はスキップされます。"
-fi
-#systemctl enable samba-ad-dc.service
-#systemctl start samba-ad-dc.service
-# サービスの起動状態を確認
-systemctl is-active $service_name > /dev/null 2>&1
-
-# サービスが停止している場合
-if [ $? -ne 0 ]; then
-  # サービスを起動
-  echo "サービス[$service_name]を起動しています..."
-  systemctl start $service_name
-
-  # サービスの起動を確認
-  systemctl is-active $service_name > /dev/null 2>&1
-
-  # サービスが起動している場合
-  if [ $? -eq 0 ]; then
-    echo "サービス[$service_name]の起動に成功しました。"
-  else
-    echo "サービス[$service_name]の起動に失敗しました。"
-  fi
-else
-  echo "サービス[$service_name]は既に起動しているため、起動処理はスキップされます。"
-fi
+service_start "samba-ad-dc.service"
 
 # DNSの設定変更 その２
 # echo -e "nameserver 127.0.0.1\nsearch ad.nayutaya.jp" | tee /etc/resolv.conf
-realm=$(echo $REALM |tr '[:upper:]' '[:lower:]')
-echo $realm
-if grep -E "search.*$realm" /etc/resolv.conf ;then
+if grep -E "search.*${fqdn#*.}" /etc/resolv.conf ;then
   echo "nothing"
 else
 	sed -i "
 /search/ {
-s/.*/& $realm/
+s/.*/& ${fqdn#*.}/
 }
 " /etc/resolv.conf
 
