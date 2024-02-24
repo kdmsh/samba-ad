5188dbe1aac7848cb78e48525ff7d10a85c015e0
kdmsh <160145510+kdmsh@users.noreply.github.com>
2024-02-24 16:10:30 +0900

    ドメインを変更

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 timezone="Asia/Tokyo"
 lang="ja_JP.UTF-8"
 ipv6=true
-fqdn=addc01.kodama-system.com
+fqdn=dc01.ad.kodama-system.com
 
 # Common Settings
 tailscale=false
 
 # Samba Setting
-REALM="ADDC01.KODAMA-SYSTEM.COM"
-domain="KODAMA-SYSTEM.COM"
+REALM="DC01.AD.KODAMA-SYSTEM.COM"
+domain="AD.KODAMA-SYSTEM.COM"
 admin_password="55Kodam@"
 
